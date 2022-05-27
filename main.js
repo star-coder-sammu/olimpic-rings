a0624addb7b1e722424605cc47a2d7eb9fd68965
star-coder-sammu <101115549+star-coder-sammu@users.noreply.github.com>
2022-05-27 10:29:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "grey".
+    Use lineWidth and set the width to 1.
+    Use rect() to create a rectangle at 150 and 143
+with 430 width and 200 height.
+*/
+
+ctx._______;
+ctx._______ = "grey";
+ctx._______ = 1;
+ctx._______(x, y, width, height);
+ctx.stroke();
+
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x = 150 and y = 143
+with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
+*/
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(210, 40, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+// Similarly, create a black circle with position 350 and 210
+
+// Similarly, create a red circle with position 210 and 40
+
+// Similarly, create an orange circle with position 300 and 250
+
+// Similarly, create a green circle with position 400 and 250
